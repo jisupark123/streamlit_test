d7f3e410aeeea4d38789845e370c04411e06e120
박지수 <jisupark2000@gmail.com>
2024-02-01 10:29:08 +0900

    First commit

---
+++ chatgpt.py
@@ -0,0 +1,44 @@
+import openai
+import streamlit as st
+
+OPENAI_API_KEY = "8c91c5c7058847f3820f9d7c12bbb1fb"
+OPENAI_API_AZURE_ENDPOINT = "https://sktfly-ai.openai.azure.com/"
+OPENAI_API_TYPE = "azure"
+OPENAI_API_VERSION = "2023-05-15"
+
+st.header("인공지능 시인 서비스", divider="rainbow")
+
+name = st.text_input("이름을 입력하세요")
+
+if name:
+    st.write(name + "님 반갑습니다.")
+
+subject = st.text_input("시의 제목을 입력하세요")
+content = st.text_input("시의 내용을 입력하세요")
+
+openai.api_key = OPENAI_API_KEY
+openai.azure_endpoint = OPENAI_API_AZURE_ENDPOINT
+openai.api_type = OPENAI_API_TYPE
+openai.api_version = OPENAI_API_VERSION
+
+
+button_result = st.button("RUN")
+
+if button_result:
+    with st.spinner("답변 생성 중..."):
+        ans = openai.chat.completions.create(
+            model="dev-gpt-35-turbo",
+            temperature=1,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "한국어로 답해줘",
+                },
+                {"role": "user", "content": f"작가의 이름은 {name}"},
+                {"role": "user", "content": f"시의 주제는 {subject}"},
+                {"role": "user", "content": f"시의 내용은 {content}"},
+                {"role": "user", "content": "이 내용으로 시를 지어줘"},
+            ],
+        )
+
+        st.write(ans.choices[0].message.content)
